0d4ecf9659683af780a03875dab59c7b983e8b67
Vraj-V <vrajv2108@gmail.com>
2026-02-18 12:05:15 +0530

    Contacting Page

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,22 +1,46 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { arrow } from '../assets/icons';
 
+ const InfoBox = ({text,link,btnText}) => (
+    <div className='info-box'>
+        <p className='font-medium sm:text-xl text-center'>{text}</p>
+        <Link to={link} className='neo-brutalism-white neo-btn'>
+            {btnText}
+                <img src={arrow} alt="arrow" className='ml-2 w-4 h-4 object-contain '/>
+        </Link>
+    </div>
+)
 
 const renderContent ={
     1:(
-        <h1>1</h1>
+        <h1 className='sm-text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8  text-white mx-5'>
+            Hi, I'm <span className='font-semibold'>Vraj</span>👋
+            <br />
+            A Software Engineer from India.
+        </h1>
     ),
     2:(
-        <h1>2</h1>
+        <InfoBox 
+            text="Worked with many companies and learned many skills along the journey" 
+            link="/about"
+            btnText="Learn more"/>
     ),
     3:(
-        <h1>3</h1>
+        <InfoBox
+            text="Led multiple projects to success over the year. Curious about the impact?"
+            link="/projects"
+            btnText="Visit my projects"/>
     ),
     4:(
-        <h1>4</h1>
+        <InfoBox 
+            text="Need a developer for your next project? Let's connect and create something amazing together!" 
+            link="/contact" 
+            btnText="Let`s talk"/>
     ),
 }
 const HomeInfo = ({currentStage}) => {
-  return renderContent[currentStage] || null;
+    return renderContent[currentStage] || null;
 }
 
 export default HomeInfo
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,92 @@
-import React from 'react'
+import React,{useState,useRef} from 'react'
+import { Link } from 'react-router-dom';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+  const [isLoading,setIsLoading] = useState(false);
+  const formRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    setIsLoading(true);
+    console.log(formData);
+
+    emailjs.sendForm('service_opnckif', 
+      'template_9maa6ip', 
+      formRef.current, 'user_2XoLZt3mKzqj8aGQy7b2')
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleFocus = () => {};
+  const handleBlur = () => {};
+
   return (
-    <div>Contact</div>
+    <section className='relative flex lg:flex-row flex-col max-container'>
+      <div className='flex-1 min-w[50%] flex flex-col'>
+        <h1 className='head-text font-bold'>Get In Touch</h1>
+
+        <form ref={formRef} action="" onSubmit={handleSubmit} className='w-full flex flex-col gap-7 mt-14'>
+        <label className='text-black-500 font-semibold'>Name:</label>
+        <input 
+          type="text" 
+          id="name" 
+          name="name"
+          placeholder='Name'
+          value={formData.name}
+          onChange={handleChange}
+          className='p-3'
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+        />
+
+        <label htmlFor="email">Email:</label>
+        <input 
+          type="email" 
+          id="email" 
+          name="email" 
+          placeholder='John@email.com'
+          value={formData.email}
+          onChange={handleChange}
+          className='p-3'
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+        />
+
+        <label htmlFor="message">Message:</label>
+        <textarea 
+          id="message" 
+          name="message" 
+          rows={4}
+            placeholder='Your Message'
+          value={formData.message}
+          onChange={handleChange}
+          className='p-3 h-32 resize-none'
+          onFocus={handleFocus}
+          onBlur={handleBlur}   
+        ></textarea>
+
+        <button 
+        type="submit" 
+        className='btn'
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        disabled={isLoading}
+       >
+        {isLoading ? 'Sending...' : 'Send Message'}
+        </button>
+        </form>
+      </div>
+    </section>
   )
 }
 
